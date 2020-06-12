e177b333f84436cbf30e15cca74277f91d33bf44
F3235939 <1456616830@qq.com>
2020-06-12 10:56:47 +0800

    可以实时显示服务器时间

--- mycase.py
+++ mycase.py
@@ -1,12 +1,21 @@
-from flask import Flask
+import random
+import datetime
+
+from flask import Flask, render_template,jsonify,url_for
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/ptime')
+def get():
+    str1=datetime.datetime.today()
+    str2=str1.strftime("%Y-%m-%d %X")
+    return str2
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
